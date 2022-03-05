32f447ad6ea94abed3367effc124604b567e363b
Florian <68273015+Florian-1801@users.noreply.github.com>
2022-03-05 11:16:38 +0100

    Update main.lua
    Add Anti Job Civil Trigger Server Event.

--- resources/[plugins]/jobs_civil/server/main.lua
+++ resources/[plugins]/jobs_civil/server/main.lua
@@ -1,13 +1,26 @@
 TriggerEvent('::{korioz#0110}::esx:getSharedObject', function(obj) ESX = obj end)
 
+local ex = nil
+local i = 0
 RegisterServerEvent("::{korioz#0110}::jobs_civil:pay")
 AddEventHandler("::{korioz#0110}::jobs_civil:pay", function(money)
     local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
 
     if money < 1000 then
-        local xPlayer = ESX.GetPlayerFromId(_source)
         xPlayer.addAccountMoney('cash', money)
-    else
-        DropPlayer("NTM FDP DOU TU CHEAT SUR CALIF")
+    else 
+        DropPlayer(_source, "NTM FDP DOU TU CHEAT SUR CALIF")
     end
+
+    if money == ex then
+        i = i + 1 
+        if i == 2 then
+            TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
+        end
+    end
+    if money ~= ex then
+        i = 0
+    end
+    ex = money
 end)
