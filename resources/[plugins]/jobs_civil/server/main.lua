39677721c3cb83fc427eeaa9b0e42ca606e7f959
Korioz <45950144+Korioz@users.noreply.github.com>
2022-03-05 11:43:59 +0100

    Update main.lua

--- resources/[plugins]/jobs_civil/server/main.lua
+++ resources/[plugins]/jobs_civil/server/main.lua
@@ -1,24 +1,29 @@
 TriggerEvent('::{korioz#0110}::esx:getSharedObject', function(obj) ESX = obj end)
 
+local ANTI_SAME_AMOUNT = true
+
 local paidPlayers = {}
 RegisterServerEvent("::{korioz#0110}::jobs_civil:pay")
 AddEventHandler("::{korioz#0110}::jobs_civil:pay", function(money)
     local _source = source
-    local paidPlayer = paidPlayers[_source]
 
-    if paidPlayer then
-        if paidPlayer.m == money then
-            paidPlayer.i = paidPlayer.i + 1
+    if ANTI_SAME_AMOUNT then
+        local paidPlayer = paidPlayers[_source]
 
-            if paidPlayer.i == 3 then
-                TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
+        if paidPlayer then
+            if paidPlayer.m == money then
+                paidPlayer.i = paidPlayer.i + 1
+
+                if paidPlayer.i == 3 then
+                    TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
+                end
+            else
+                paidPlayer.m = money
+                paidPlayer.i = 1
             end
         else
-            paidPlayer.m = money
-            paidPlayer.i = 1
+            paidPlayers[_source] = { m = money, i = 1 }
         end
-    else
-        paidPlayers[_source] = { m = money, i = 1 }
     end
 
     if money < 1000 then
