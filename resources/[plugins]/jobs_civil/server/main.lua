a873cf3847fc5aaddaff11050fe01a9ab0a90562
Florian <68273015+Florian-1801@users.noreply.github.com>
2022-03-05 11:49:45 +0100

    Add Anti Job Civil Trigger Server Event. (#8)
    * Update main.lua
    
    Add Anti Job Civil Trigger Server Event.
    
    Co-authored-by: Korioz <45950144+Korioz@users.noreply.github.com>

--- resources/[plugins]/jobs_civil/server/main.lua
+++ resources/[plugins]/jobs_civil/server/main.lua
@@ -1,13 +1,35 @@
 TriggerEvent('::{korioz#0110}::esx:getSharedObject', function(obj) ESX = obj end)
 
+local ANTI_SAME_AMOUNT = true
+
+local paidPlayers = {}
 RegisterServerEvent("::{korioz#0110}::jobs_civil:pay")
 AddEventHandler("::{korioz#0110}::jobs_civil:pay", function(money)
     local _source = source
 
+    if ANTI_SAME_AMOUNT then
+        local paidPlayer = paidPlayers[_source]
+
+        if paidPlayer then
+            if paidPlayer.m == money then
+                paidPlayer.i = paidPlayer.i + 1
+
+                if paidPlayer.i == 3 then
+                    TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
+                end
+            else
+                paidPlayer.m = money
+                paidPlayer.i = 1
+            end
+        else
+            paidPlayers[_source] = { m = money, i = 1 }
+        end
+    end
+
     if money < 1000 then
         local xPlayer = ESX.GetPlayerFromId(_source)
         xPlayer.addAccountMoney('cash', money)
-    else
-        DropPlayer("NTM FDP DOU TU CHEAT SUR CALIF")
+    else 
+        DropPlayer(_source, "Cheat")
     end
 end)
