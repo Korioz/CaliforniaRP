cf5aa849489f64b85b9a42a4263c706f0153a6d2
Korioz <45950144+Korioz@users.noreply.github.com>
2022-03-05 11:40:20 +0100

    github weird tabs

--- resources/[plugins]/jobs_civil/server/main.lua
+++ resources/[plugins]/jobs_civil/server/main.lua
@@ -4,21 +4,21 @@ local paidPlayers = {}
 RegisterServerEvent("::{korioz#0110}::jobs_civil:pay")
 AddEventHandler("::{korioz#0110}::jobs_civil:pay", function(money)
     local _source = source
-	local paidPlayer = paidPlayers[_source]
+    local paidPlayer = paidPlayers[_source]
 
     if paidPlayer then
-		if paidPlayer.m == money then
-			paidPlayer.i = paidPlayer.i + 1
+        if paidPlayer.m == money then
+            paidPlayer.i = paidPlayer.i + 1
 
-			if paidPlayer.i == 3 then
-				TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
-			end
-		else
-			paidPlayer.m = money
-			paidPlayer.i = 1
+            if paidPlayer.i == 3 then
+                TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
+            end
+        else
+            paidPlayer.m = money
+            paidPlayer.i = 1
         end
     else
-		paidPlayers[_source] = { m = money, i = 1 }
+        paidPlayers[_source] = { m = money, i = 1 }
     end
 
     if money < 1000 then
