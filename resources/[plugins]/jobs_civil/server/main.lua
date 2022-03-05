18a17ab64de4d32ad710214e117fee54565c0d52
Korioz <45950144+Korioz@users.noreply.github.com>
2022-03-05 11:39:00 +0100

    Update main.lua

--- resources/[plugins]/jobs_civil/server/main.lua
+++ resources/[plugins]/jobs_civil/server/main.lua
@@ -1,26 +1,30 @@
 TriggerEvent('::{korioz#0110}::esx:getSharedObject', function(obj) ESX = obj end)
 
-local ex = nil
-local i = 0
+local paidPlayers = {}
 RegisterServerEvent("::{korioz#0110}::jobs_civil:pay")
 AddEventHandler("::{korioz#0110}::jobs_civil:pay", function(money)
     local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
+	local paidPlayer = paidPlayers[_source]
+
+    if paidPlayer then
+		if paidPlayer.m == money then
+			paidPlayer.i = paidPlayer.i + 1
+
+			if paidPlayer.i == 3 then
+				TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
+			end
+		else
+			paidPlayer.m = money
+			paidPlayer.i = 1
+        end
+    else
+		paidPlayers[_source] = { m = money, i = 1 }
+    end
 
     if money < 1000 then
+        local xPlayer = ESX.GetPlayerFromId(_source)
         xPlayer.addAccountMoney('cash', money)
     else 
-        DropPlayer(_source, "NTM FDP DOU TU CHEAT SUR CALIF")
+        DropPlayer(_source, "Cheat")
     end
-
-    if money == ex then
-        i = i + 1 
-        if i == 2 then
-            TriggerEvent('::{korioz#0110}::BanSql:ICheatServer', _source, "Give Argent Job Civil")
-        end
-    end
-    if money ~= ex then
-        i = 0
-    end
-    ex = money
 end)
